2c233c31fc4e2055b961ddf4294bc82f33505e97
vasubabu <vasu1188@gmail.com>
2018-12-09 14:11:19 +0530

    first commit

---
+++ spiproto.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1297b0a9ff3bc41e0ddf9006951f8f413c5cf1323adaa09fe2e8449d0afceae
+size 953
---
+++ spitest.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f2f2920d69874dda71319180589d0ef6ee8feee6eb96befc49086587f86ef6f
+size 7347
